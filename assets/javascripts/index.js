b4a226842f8ff0b9b2553d09d2747863e37b5d9f
Raihan <mraihanm99@gmail.com>
2023-06-26 11:24:59 +0800

    first commit

---
+++ assets/javascripts/index.js
@@ -0,0 +1,27 @@
+$(function() {
+  $(".owl-carousel").owlCarousel({
+    items: 2,
+    margin: 10,
+    loop: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    navSpeed: 10,
+    // center: true,
+    rtl: true,
+  });
+
+  let hamburger = $("button.hamburger");
+  if (hamburger.length) {
+    hamburger.click(function() {
+      const nav = $(this).parent().find("nav ul");
+
+      if (this.classList.contains("is-active")) {
+        nav.addClass("is-active");
+      } else {
+        nav.removeClass("is-active");
+      }
+    });
+  }
+
+  AOS.init();
+})
