6664d947bd14d61a1933c91636bf147c3583cdea
Raihan <mraihanm99@gmail.com>
2023-07-23 12:46:11 +0800

    finalize.

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,18 +1,28 @@
-$(function() {
+$(function () {
   $(".owl-carousel").owlCarousel({
-    items: 2,
-    margin: 10,
     loop: true,
     autoplay: true,
     autoplayHoverPause: true,
     navSpeed: 10,
     // center: true,
     rtl: true,
+    responsive: {
+      300: {
+        items: 1,
+        margin: 10,
+        center: true,
+        autoWidth: true,
+      },
+      768: {
+        items: 2,
+        margin: 10,
+      },
+    },
   });
 
   let hamburger = $("button.hamburger");
   if (hamburger.length) {
-    hamburger.click(function() {
+    hamburger.click(function () {
       const nav = $(this).parent().find("nav ul");
 
       if (this.classList.contains("is-active")) {
@@ -28,4 +38,32 @@ $(function() {
   if (window.innerWidth <= 767) {
     $("section#description article div.image").removeAttr("data-aos-anchor-placement");
   }
-})
+
+  $(".nav-item a").click(function () {
+    const sectionId = this.dataset.id;
+    const sectionEl = $(`section#${sectionId}`);
+    const headerHeight = $("header")[0].offsetHeight;
+    const offsetTop = sectionEl.offset().top - headerHeight;
+    $(".nav-item.active").removeClass("active");
+    $(this).parent().addClass("active");
+
+    window.scrollTo({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  });
+
+  $(".get-to-know").click(function () {
+    const sectionEl = $("section#description");
+    const headerHeight = $("header")[0].offsetHeight;
+    const offsetTop = sectionEl.offset().top - headerHeight;
+    window.scrollTo({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  });
+
+  $(".order-button").click(function () {
+    window.open("https://wa.me/085792138348", "_blank");
+  });
+});
